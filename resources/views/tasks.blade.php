4c79faf2460663be04536f56750b3963c8e43914
Andrey NESTEROV <andrey.nesterov@laposte.net>
2021-05-26 11:36:25 +0200

    fix routing, updating code

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,68 +1,58 @@
-@extends('layout.app')
+@extends('app')
 
 @section('content')
     <div class="panel-body">
-        @include('common.errors')
+        @include('errors')
 
-        <form action="{{ url('tasks') }}" method="post" class="form-horizontal">
-            {{-- against CSRF attacks, we need to include empty CSRF field --}}
+        <form action="{{url('tasks')}}" method="post" class="form-horizontal">
             {{ csrf_field() }}
-        </form>
-
-        <form action="{{ url('tasks->id') }}" method="post">
-            {{-- against CSRF attacks, we need to include empty CSRF field --}}
-            {{ csrf_field() }}
-            {!!method_field('DELETE')!!}
-            <button class ="btn btn-danger">
-                <i class= "fa fa-trash">Delete</i>
-            </button>
-        </form>
-
-        @if (count($tasks) > 0)
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Current Tasks
-                </div>
-                <div class="panel-body">
-                    <table class="table table-striped task-table">
-                        <thead>
-                            <th>Task</th>
-                            <th>&nbsp;</th>
-                        </thead>
-                        <tbody>
-                            @foreach ($tasks as $task)
-                                <tr>
-                                    <td class="table-text">
-                                        <div>{{ $task->name }}</div>
-                                    </td>
-                                    <td>&nbsp;</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+            <div class="form-group">
+                <label for="task" class="col-sm-3 control-label">Task</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" id="task" name="name">
                 </div>
             </div>
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Task
+                    </button>
+                </div>
+            </div>
+        </form>
+
+        @if (count($tasks)>0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($tasks as $task)
+                        <tr>
+                            <td class="table-text">
+                                <div>{{$task->name}}</div>
+                            </td>
+                            <td>
+                                <form action="{{ url('tasks/'.$task->id) }}" method="post">
+                                    {{ csrf_field() }}
+                                    {!! method_field('DELETE') !!}
+                                    <button class="btn btn-danger">
+                                        <i class="fa fa-trash"></i> Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
         @endif
     </div>
 @endsection
-
-
-{{--  
-old code
-
-<div class="form-group">
-    <label for="task" class="col-sm-3 control-label">Task</label>
-    <div class="col-sm-6">
-        <input type="text" class="form-control" name="name" id="task">
-    </div>
-</div>
-<div class="form-group">
-    <div class="col-sm-offset-3 col-sm-6">
-        <button class="btn btn-default">
-            <i class="fa fa-plus"></i>Add Task
-        </button>
-    </div>
-</div>
-
-</div>
---}}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
-
+use app\tasks;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,26 +17,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => 'web'], function (){
+
     Route::get('/', function () {
-    $tasks = Tasks::orderBy('created_at', 'asc')->get();
-        return view ('tasks', ["tasks"=>$tasks]);
- });
+        $tasks = tasks::orderBy('created_at', 'asc')->get();
+        return view('tasks', ["tasks" => $tasks]);
+    });
+
     Route::post('/tasks', function (Request $request) {
-    //
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max255'
-    ]);
-    if ($validator -> fails()){
-        return redirect('/')->withInput();
-    }
-    $task = new Tasks();
-    $task -> name = $request -> name;
-    $task -> save();
-    
-    return redirect('/'); 
- });
-     Route::delete('/tasks{task}', function (Tasks $task) {
-    $task ->delete();
-    return redirect('/');
- });
+        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if($validator->fails()){
+            return redirect('/')->withInput();
+        }
+
+        $task = new tasks();
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    Route::delete('/tasks/{task}', function (tasks $task) {
+        $task->delete();
+        return redirect('/');
+    });
+
+
+
 });
